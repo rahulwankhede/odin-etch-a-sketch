3fbeb50b2ae16edd02b1830976a07e89ef06b82f
Rahul Wankhede <rahulwankhede@github.com>
2022-12-27 01:49:56 +0530

    make div squares of 40px with red color and 0.2 rem gap between them

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ let div;
 for(let i = 0; i < gridSize; i++){
 	for(let j = 0; j < gridSize; j++){
 		div = document.createElement('div');
-		div.innerText = i*gridSize + j + 1;
+		div.setAttribute("class", "box");
+		const id = i*gridSize + j + 1;
+		div.setAttribute("id", `${id}`)
+		div.innerText = id;
 		container.appendChild(div);
 	}
 }
