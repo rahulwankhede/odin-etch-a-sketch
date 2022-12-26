5b019e4fab56dc9fb39d8413e353984d68807093
Rahul Wankhede <rahulwankhede@github.com>
2022-12-27 01:21:54 +0530

    add 16x16 divs to container

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 const container = document.querySelector(".container");
 
+const gridSize = 16;
 
+let div;
+
+for(let i = 0; i < gridSize; i++){
+	for(let j = 0; j < gridSize; j++){
+		div = document.createElement('div');
+		div.innerText = i*gridSize + j + 1;
+		container.appendChild(div);
+	}
+}
