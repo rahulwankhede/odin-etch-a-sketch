b8fa9c4fb5afedc70c710869f3130b737704e270
Rahul Wankhede <rahulwankhede@github.com>
2022-12-27 03:47:03 +0530

    change color to green when hovering over a grid element

--- script.js
+++ script.js
@@ -2,14 +2,20 @@ const container = document.querySelector(".container");
 
 const gridSize = 16;
 
-let div;
+let div, id;
 
 for(let i = 0; i < gridSize; i++){
 	for(let j = 0; j < gridSize; j++){
 		div = document.createElement('div');
 		div.setAttribute("class", "box");
-		const id = i*gridSize + j + 1;
+		id = i*gridSize + j + 1;
 		div.setAttribute("id", `${id}`)
 		container.appendChild(div);
+		div.addEventListener('mouseover', function(e){
+			this.classList.add("hover");
+		});
+		div.addEventListener('mouseout', function(e){
+			this.classList.remove("hover");
+		});
 	}
 }
