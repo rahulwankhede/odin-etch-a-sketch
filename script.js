41834a6d674a966e2f687bb2c1ff94d2b61d9404
Rahul Wankhede <rahulwankhede@github.com>
2022-12-27 22:04:03 +0530

    add button that prompts user for new size and redraws the grid

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ const container = document.querySelector(".container");
 
 function drawGrid(size){
 	let div, id;
-
+	container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
 	for(let i = 0; i < size; i++){
 		for(let j = 0; j < size; j++){
 			div = document.createElement('div');
 			div.setAttribute("class", "box");
 			id = i*size + j + 1;
-			div.setAttribute("id", `${id}`)
+			div.setAttribute("id", `${id}`);
 			container.appendChild(div);
 			div.addEventListener('mouseover', function(e){
 				this.classList.add("hover");
@@ -20,12 +20,19 @@ function drawGrid(size){
 	}
 }
 
-let gridSize = 16;
-drawGrid(gridSize);
-
 function getGridSize(){
 	return parseInt(prompt("Input new grid size not greater than 100"));
 }
 
+function redrawGrid(e){
+	let gridSize = getGridSize();
+	container.replaceChildren();
+	drawGrid(gridSize);
+}
+
+
 const button = document.querySelector('button');
-button.addEventListener('click', getGridSize);
+button.addEventListener('click', redrawGrid);
+
+let gridSize = 16;
+drawGrid(gridSize);
